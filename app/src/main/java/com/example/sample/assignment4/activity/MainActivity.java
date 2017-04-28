03daf2dee11924052e5fb8e459046bbd643b49c1
darknight <pramod.garg@theroboticshomepage.com>
2017-04-28 15:54:55 +0530

    Intial Commit

---
+++ app/src/main/java/com/example/sample/assignment4/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sample.assignment4.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.sample.assignment4.R;
+
+/**
+ * Defualt activity
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/sample/assignment4/fragment/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.example.sample.assignment4.fragment;
+
+/**
+ * Created by darknight on 28/4/17.
+ */
+
+public class BaseFragment {
+}
